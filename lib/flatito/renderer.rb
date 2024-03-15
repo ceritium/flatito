47775ab410518553f08215bb9c57d8995e8f45b1
José Galisteo <ceritium@gmail.com>
2024-03-15 10:06:52 +0100

    Line numbers in yellow for better visibility

--- lib/flatito/renderer.rb
+++ lib/flatito/renderer.rb
@@ -47,7 +47,7 @@ module Flatito
     end
 
     def print_item(item, line_number_padding)
-      line_number = colorize("#{item.line.to_s.rjust(line_number_padding)}: ", :light_yellow)
+      line_number = colorize("#{item.line.to_s.rjust(line_number_padding)}: ", :yellow)
       value = if item.value.length.positive?
                 colorize("=> #{item.value}", :gray)
               else
